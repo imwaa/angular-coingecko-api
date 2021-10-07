68f6e989d1e99a3bb3d9eaeef84cd69a10bcc6f9
Walid Salhi <walid.dev@outlook.fr>
2021-10-07 09:07:51 +0200

    Getting All Coins market with Get

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+interface Coin {
+  id: string;
+  name: string;
+  symbol: string;
+  image: string;
+  current_price: number;
+  price_change_percetange_24: number;
+  total_volume: number;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'angular-coingecko-api';
+export class AppComponent implements OnInit {
+
+  coins: Coin[] = [];
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  ngOnInit() {
+
+    this.http.get<Coin[]>('https://api.coingecko.com/api/v3/coins/markets?vs_currency=eur&order=market_cap_desc&per_page=100&page=1&sparkline=false').subscribe(res => {
+      console.log(res);
+      this.coins = res;
+    }, (err) => console.log(err));
+
+  }
+
 }
